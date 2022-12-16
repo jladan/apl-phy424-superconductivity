f65b42117fe089d6d828251754b778821ebda332
John Ladan <jladan@physics.utoronto.ca>
2022-12-16 22:09:29 +0000

    add get_T and get_V methods to sim922

--- src/sim922.py
+++ src/sim922.py
@@ -2,8 +2,32 @@ import serial
 
 
 class SIM922(serial.Serial):
-    def __init__(self, port='COM4', baud=57600, timeout=0.5):
+    """ Class for communicating to the SIM922 diode temperature monitor
+
+    When the class is constructed, communication is started with the SIM922 module 
+    in the SIM900 mainframe through the specified serial port.
+
+    Common methods:
+        .get_T() : retrieve the temperature in K (as calculated from the calibration curve)
+        .get_V() : retrieve the voltage in mV
+        .send(command) : send the command string to the module (strings are sanitized)
+        .send_receive(query) : send the query and return the result as a bytestring
+    """
+    
+    def __init__(self, port='COM4', module_no=4, term_string='xxyyzz', baud=57600, timeout=0.5):
         super().__init__(port, baud, timeout=timeout)
+        self.term_string=term_string
+        self._connect_to_module(module_no)
+
+    def _connect_to_module(self, module_no):
+        """ Connect to the module <module_no> to pass-through commands
+        """
+        self.send(f'conn {module_no}, "{self.term_string}"')
+    
+    def _disconnect_module(self):
+        """ Send the connection termination string to return to SIM900 communication
+        """
+        self.send(self.term_string)
 
     def get_T(self):
         return float(self.send_receive('tval? 1'))
